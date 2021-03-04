65edbcab7803a5217040517d16b5a66a31a4f093
agu18044 <78278462+agu18044@users.noreply.github.com>
2021-03-04 00:41:53 -0600

    codigo para comunicación i2c

--- MCU_PIC.X/I2C.h
+++ MCU_PIC.X/I2C.h
@@ -1,78 +1,67 @@
-/* Microchip Technology Inc. and its subsidiaries.  You may use this software 
- * and any derivatives exclusively with Microchip products. 
- * 
- * THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS".  NO WARRANTIES, WHETHER 
- * EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED 
- * WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A 
- * PARTICULAR PURPOSE, OR ITS INTERACTION WITH MICROCHIP PRODUCTS, COMBINATION 
- * WITH ANY OTHER PRODUCTS, OR USE IN ANY APPLICATION. 
- *
- * IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
- * INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
- * WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS 
- * BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE.  TO THE 
- * FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS 
- * IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF 
- * ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
- *
- * MICROCHIP PROVIDES THIS SOFTWARE CONDITIONALLY UPON YOUR ACCEPTANCE OF THESE 
- * TERMS. 
- */
-
-/* 
- * File:   
- * Author: 
- * Comments:
- * Revision history: 
+ /*
+ * File            : I2C.h
+ * Author          : Ligo George
+ * Company         : electroSome
+ * Project         : I2C Library for MPLAB XC8
+ * Microcontroller : PIC 16F877A
+ * Created on April 15, 2017, 5:59 PM
+ * Link: https://electrosome.com/i2c-pic-microcontroller-mplab-xc8/
+ * Modificada por: Pablo Mazariegos con la ayuda del auxiliar Gustavo Ordoñez 
+ * Basado en Link: http://microcontroladores-mrelberni.com/i2c-pic-comunicacion-serial/
  */
 
 // This is a guard condition so that contents of this file are not included
 // more than once.  
-#ifndef XC_HEADER_TEMPLATE_H
-#define	XC_HEADER_TEMPLATE_H
+#ifndef __I2C_H
+#define	__I2C_H
 
 #include <xc.h> // include processor files - each processor file is guarded.  
+#include <pic16f887.h>
+#include <stdint.h>
 
-// TODO Insert appropriate #include <>
+#ifndef _XTAL_FREQ
+#define _XTAL_FREQ 8000000
+#endif
 
-// TODO Insert C++ class definitions if appropriate
-
-// TODO Insert declarations
-
-// Comment a function and leverage automatic documentation with slash star star
-/**
-    <p><b>Function prototype:</b></p>
-  
-    <p><b>Summary:</b></p>
-
-    <p><b>Description:</b></p>
-
-    <p><b>Precondition:</b></p>
-
-    <p><b>Parameters:</b></p>
-
-    <p><b>Returns:</b></p>
-
-    <p><b>Example:</b></p>
-    <code>
- 
-    </code>
-
-    <p><b>Remarks:</b></p>
- */
-// TODO Insert declarations or function prototypes (right here) to leverage 
-// live documentation
-
-#ifdef	__cplusplus
-extern "C" {
-#endif /* __cplusplus */
-
-    // TODO If C++ is being used, regular C code needs function names to have C 
-    // linkage so the functions can be used by the c code. 
-
-#ifdef	__cplusplus
-}
-#endif /* __cplusplus */
-
-#endif	/* XC_HEADER_TEMPLATE_H */
+//*****************************************************************************
+// Función para inicializar I2C Maestro
+//*****************************************************************************
+void I2C_Master_Init(const unsigned long c);
+//*****************************************************************************
+// Función de espera: mientras se esté iniciada una comunicación,
+// esté habilitado una recepción, esté habilitado una parada
+// esté habilitado un reinicio de la comunicación, esté iniciada
+// una comunicación o se este transmitiendo, el IC2 PIC se esperará
+// antes de realizar algún trabajo
+//*****************************************************************************
+void I2C_Master_Wait(void);
+//*****************************************************************************
+// Función de inicio de la comunicación I2C PIC
+//*****************************************************************************
+void I2C_Master_Start(void);
+//*****************************************************************************
+// Función de reinicio de la comunicación I2C PIC
+//*****************************************************************************
+void I2C_Master_RepeatedStart(void);
+//*****************************************************************************
+// Función de parada de la comunicación I2C PIC
+//*****************************************************************************
+void I2C_Master_Stop(void);
+//*****************************************************************************
+//Función de transmisión de datos del maestro al esclavo
+//esta función devolverá un 0 si el esclavo a recibido
+//el dato
+//*****************************************************************************
+void I2C_Master_Write(unsigned d);
+//*****************************************************************************
+//Función de recepción de datos enviados por el esclavo al maestro
+//esta función es para leer los datos que están en el esclavo
+//*****************************************************************************
+unsigned short I2C_Master_Read(unsigned short a);
+//*****************************************************************************
+// Función para inicializar I2C Esclavo
+//*****************************************************************************
+void I2C_Slave_Init(uint8_t address);
+//*****************************************************************************
+#endif	/* __I2C_H */
 
--- MCU_PIC.X/main.c
+++ MCU_PIC.X/main.c
@@ -7,7 +7,106 @@
 
 
 #include <xc.h>
+#include <stdint.h>
+#include <stdio.h>
+#include "I2C.h"
 
+//******************************************************************************
+//  Palabra de cofiguración
+//******************************************************************************
+// CONFIG1
+#pragma config FOSC = EXTRC_NOCLKOUT        // Oscillator Selection bits (XT oscillator: Crystal/resonator on RA6/OSC2/CLKOUT and RA7/OSC1/CLKIN)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled and can be enabled by SWDTEN bit of the WDTCON register)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config MCLRE = OFF      // RE3/MCLR pin function select bit (RE3/MCLR pin function is digital input, MCLR internally tied to VDD)
+#pragma config CP = OFF         // Code Protection bit (Program memory code protection is disabled)
+#pragma config CPD = OFF        // Data Code Protection bit (Data memory code protection is disabled)
+#pragma config BOREN = OFF      // Brown Out Reset Selection bits (BOR disabled)
+#pragma config IESO = OFF       // Internal External Switchover bit (Internal/External Switchover mode is disabled)
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enabled bit (Fail-Safe Clock Monitor is disabled)
+#pragma config LVP = OFF        // Low Voltage Programming Enable bit (RB3 pin has digital I/O, HV on MCLR must be used for programming)
+
+// CONFIG2
+#pragma config BOR4V = BOR40V   // Brown-out Reset Selection bit (Brown-out Reset set to 4.0V)
+#pragma config WRT = OFF        // Flash Program Memory Self Write Enable bits (Write protection off)
+
+//******************************************************************************
+//  Variables
+//******************************************************************************
+#define _XTAL_FREQ 8000000
+int seg = 00;
+int min = 00;
+int hora = 00;
+int dia = 00;
+int mes = 00;
+int year = 00;
+uint8_t segundos = 0;
+
+//******************************************************************************
+//  Prototipos de funciones
+//******************************************************************************
+void setup(void);
+void set_tiempo (void);
+uint8_t bcd_a_dec (uint8_t valor);
+uint8_t dec_a_bcd (uint8_t valor);
+
+
+//******************************************************************************
+//  Ciclo principal
+//******************************************************************************
 void main(void) {
-    return;
+    setup();
+    set_tiempo();
+    while (1){
+        I2C_Master_Start();
+        I2C_Master_Write(0xD0);
+        I2C_Master_Write(0);
+        I2C_Master_Stop();
+        
+        I2C_Master_Start();
+        I2C_Master_Write(0xD1);
+        seg = (I2C_Master_Read(1));
+        I2C_Master_Read(1);
+        I2C_Master_Stop();
+        
+        I2C_Master_Start();
+        I2C_Master_Write(0xD1);
+        I2C_Master_Write(0);
+        I2C_Master_Stop();
+        __delay_ms(200);
+        
+        segundos = bcd_a_dec(seg);
+      
+    }
+}
+uint8_t bcd_a_dec (uint8_t valor){
+    return((valor >> 4)*10+(valor & 0x0F));
+}
+
+uint8_t dec_a_bcd (uint8_t valor){
+    return(((valor/10) << 4) + (valor % 10));
+}
+
+void set_tiempo (void){
+    I2C_Master_Start();
+    I2C_Master_Write(0xD0);
+    I2C_Master_Write(dec_a_bcd(seg));
+    I2C_Master_Write(dec_a_bcd(min));
+    I2C_Master_Write(dec_a_bcd(hora));
+    I2C_Master_Write (1);
+    I2C_Master_Write(dec_a_bcd(dia));
+    I2C_Master_Write(dec_a_bcd(mes));
+    I2C_Master_Write(dec_a_bcd(year));
+    I2C_Master_Stop();
+    __delay_ms(200);
+}
+
+//******************************************************************************
+//  Cofiguración
+//******************************************************************************
+void setup(void) {
+    ANSEL = 0;
+    ANSELH = 0;
+    
+    I2C_Master_Init(100000);        // Inicializar Comuncación I2C
 }
